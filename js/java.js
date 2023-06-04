2a1101e2473819303abfa9be336d241a56c5b5b7
HugoH <Hhervellagonzalez@gmail.com>
2023-06-04 19:19:45 +0200

    Arreglo y funcinamiento de js y mas productos

--- js/java.js
+++ js/java.js
@@ -8,44 +8,37 @@ window.addEventListener('scroll', function() {
       barra2.classList.remove('sticky');
     }
   });
-
-
-function App() {}
-
-    window.onload = function(event){
-        var app = new App();
-        window.app = app;
-
-    }
-
+  
+  function App() {
     App.prototype.processingButton = function(event) {
-        const btn = event.currentTarget;
-        const carruselList = event.currenttarget.parentNode;
-        const track = event.currentTarget.parentNode.querySelector('#track')
-        const carrusel = track.querySelectorAll('.carrusel');
-
-        const carruselWidth = carrusel[0].offsetWidth;
-
-        const trackWidth = track.offsetWidth;
-        const listWidth = carruselList.offsetWidth;
-        
-        track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.styIe.left.slice(0, -2) * -1);
-        btn.dataset.button == "button-prev" ? prevAction(leftPosition, carruselWidth, track) : nextAction(leftPosition,trackWidth, listWidth, carruselWidth, track);
-        
+      const btn = event.currentTarget;
+      const carruselList = event.currentTarget.parentNode;
+      const track = event.currentTarget.parentNode.querySelector('#track');
+      const carrusel = track.querySelectorAll('.carrusel');
+  
+      const carruselWidth = carrusel[0].offsetWidth;
+  
+      const listWidth = carruselList.offsetWidth;
+      const trackWidth = track.offsetWidth;
+  
+      track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
+      btn.dataset.button == "button-prev" ? prevAction(leftPosition, carruselWidth, track) : nextAction(leftPosition, trackWidth, listWidth, carruselWidth, track);
     }
-
+  
     let prevAction = (leftPosition, carruselWidth, track) => {
-        if (leftPosition > 0){
-
-            track.style.left = `${-1 *(leftPosition - carruselWidth)}px`;
-        }    
+      if (leftPosition > 0) {
+        track.style.left = `${-1 *(leftPosition - carruselWidth)}px`;
+      }
     }
-
+  
     let nextAction = (leftPosition, trackWidth, listWidth, carruselWidth, track) => {
-        if (leftPosition < (trackWidth - listWidth)){
-            
-            track.style.left = `${-1 *(leftPosition + carruselWidth)}px`;
-        }    
+      if (leftPosition < (trackWidth - listWidth)) {
+        track.style.left = `${-1 *(leftPosition + carruselWidth)}px`;
+      }
     }
-
-
+  }
+  
+  window.onload = function(event) {
+    window.app = new App();
+  }
+  
